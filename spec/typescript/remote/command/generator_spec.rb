3e25ab6bd6063136fbf162b309a68f2e6696beaa
Miles Georgi <azimux@gmail.com>
2023-11-28 19:04:36 +0000

    Create gem with RubyMine, add /.idea/ to .gitignore, and make initial commit

---
+++ spec/typescript/remote/command/generator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Typescript::Remote::Command::Generator do
+  it "has a version number" do
+    expect(Typescript::Remote::Command::Generator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

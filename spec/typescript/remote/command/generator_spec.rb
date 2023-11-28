c8d3ff2f51ba37d185ac568caf9f031dae76e976
Miles Georgi <azimux@gmail.com>
2023-11-28 19:35:12 +0000

    Get test suite and rubocop passing

--- spec/typescript/remote/command/generator_spec.rb
+++ spec/typescript/remote/command/generator_spec.rb
@@ -1,9 +1,5 @@
-RSpec.describe Typescript::Remote::Command::Generator do
+RSpec.describe Foobara::RemoteGenerator::Typescript do
   it "has a version number" do
-    expect(Typescript::Remote::Command::Generator::VERSION).to_not be_nil
-  end
-
-  it "does something useful" do
-    expect(false).to be(true)
+    expect(TypescriptRemoteCommandGenerator::Version::VERSION).to be_a(String)
   end
 end

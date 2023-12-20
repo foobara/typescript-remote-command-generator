183a9b0cc48d6bc5345852ca7f4583a6d00c620e
Miles Georgi <azimux@gmail.com>
2023-12-20 01:05:32 +0000

    1703034332

--- src/remote_generator/generate_typescript.rb
+++ src/remote_generator/generate_typescript.rb
@@ -15,14 +15,6 @@ module Foobara
         each_element_to_generate do
           generate_element
         end
-        # generate_organizations
-        # generate_domains
-        # each_command do
-        #   generate_command
-        #   generate_command_inputs
-        #   generate_command_result
-        #   generate_command_errors
-        # end
 
         generate_generated_files_json
 
@@ -96,102 +88,11 @@ module Foobara
         "#{__dir__}/templates"
       end
 
-      def generate_organizations
-        organization_manifests.each do |organization_manifest|
-          organization_generator = Services::OrganizationGenerator.new(organization_manifest)
-          paths_to_source_code[organization_generator.target_path.join("/")] = organization_generator.generate
-        end
-      end
-
-      def generate_domains
-        domain_manifests.each do |domain_manifest|
-          domain_generator = Services::DomainGenerator.new(domain_manifest)
-          paths_to_source_code[domain_generator.target_path.join("/")] =
-            domain_generator.generate
-        end
-      end
-
-      def organization_manifests
-        manifest.organizations
-      end
-
-      def domain_manifests
-        manifest.domains
-      end
-
-      def each_command
-        manifest.commands.each do |command_manifest|
-          self.command_manifest = command_manifest
-          yield
-        end
-      end
-
-      def generate_command
-        command_generator = Services::CommandGenerator.new(command_manifest)
-        paths_to_source_code[command_generator.target_path.join("/")] = command_generator.generate
-      end
-
-      def generate_command_inputs
-        command_inputs_generator = Services::CommandInputsGenerator.new(command_manifest)
-        paths_to_source_code[command_inputs_generator.target_path.join("/")] = command_inputs_generator.generate
-      end
-
-      def generate_command_result
-        command_result_generator = Services::CommandResultGenerator.new(command_manifest)
-        paths_to_source_code[command_result_generator.target_path.join("/")] = command_result_generator.generate
-      end
-
-      def generate_command_errors
-        command_errors_generator = Services::CommandErrorsGenerator.new(command_manifest)
-        paths_to_source_code[command_errors_generator.target_path.join("/")] = command_errors_generator.generate
-      end
-
       def generate_generated_files_json
         paths_to_source_code["foobara-generated.json"] = "[\n#{
           paths_to_source_code.keys.map { |k| "  \"#{k}\"" }.join(",\n")
         }\n]\n"
       end
-
-      # def generate_and_write_all
-      #   organization_manifests.each do |organization_manifest|
-      #     organization_generator = OrganizationGenerator.new(organization_manifest)
-      #     organization_generator.write
-      #   end
-      #
-      #   domain_manifests.each do |domain_manifest|
-      #     domain_generator = DomainGenerator.new(domain_manifest)
-      #     domain_generator.write
-      #   end
-      #
-      #   entity_manifests_to_generate.each do |entity_manifest|
-      #     entity_generator = EntityGenerator.new(entity_manifest)
-      #     entity_generator.write
-      #   end
-      #
-      #   global_manifest.each do |organization_name, organization_manifest|
-      #     organization_manifest.each do |domain_name, domain_manifest|
-      #       # TODO: remove this check when we're ready to generate domains/commands that have Models/Entities
-      #       next if domain_name == "Math"
-      #
-      #       domain_manifest.commands.each do |command_name, command_manifest|
-      #         command_generator = CommandGenerator.new(command_manifest)
-      #         command_generator.write
-      #
-      #         command_inputs_generator = CommandInputsGenerator.new(command_manifest)
-      #         command_inputs_generator.write
-      #
-      #         result_generator = CommandResultGenerator.new(command_manifest)
-      #         result_generator.write
-      #
-      #         error_types_generator = CommandErrorsGenerator.new(command_manifest)
-      #         error_types_generator.write
-      #       end
-      #     end
-      #  end
-      # end
-      #
-      # TODO: move this to a mixin somehow?
-      #
     end
   end
 end

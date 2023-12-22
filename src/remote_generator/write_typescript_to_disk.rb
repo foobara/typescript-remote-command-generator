41b5367e21ed9e37c465353a8caf3b13fec4f241
Miles Georgi <azimux@gmail.com>
2023-12-21 18:41:32 -0800

    1703212892

--- src/remote_generator/write_typescript_to_disk.rb
+++ src/remote_generator/write_typescript_to_disk.rb
@@ -9,16 +9,25 @@ module Foobara
       inputs raw_manifest: { type: :associative_array },
              manifest_url: { type: :string },
              output_directory: { type: :string, required: true }
+      result :associative_array
 
       depends_on GenerateTypescript
 
       def execute
         generate_typescript
+        delete_old_files_if_needed
+        write_all_files_to_disk
 
-        write_to_disk
+        paths_to_source_code
       end
 
-      def write_all_to_tmp(result)
+      attr_accessor :paths_to_source_code
+
+      def generate_typescript
+        self.paths_to_source_code = run_subcommand!(GenerateTypescript, raw_manifest:, manifest_url:)
+      end
+
+      def delete_old_files_if_needed
         file_list_file = "#{output_directory}/foobara-generated.json"
 
         if File.exist?(file_list_file)
@@ -32,16 +41,18 @@ module Foobara
 
           FileUtils.rm_rf(file_list_file)
         end
+      end
 
-        write_to_tmp("foobara-generated.json", result["foobara-generated.json"])
+      def write_all_files_to_disk
+        write_to_tmp("foobara-generated.json", paths_to_source_code["foobara-generated.json"])
 
-        result.map do |path, contents|
-          Thread.new { write_to_tmp(path, contents) unless path == "foobara-generated.json" }
+        paths_to_source_code.map do |path, contents|
+          Thread.new { write_file_to_disk(path, contents) unless path == "foobara-generated.json" }
         end.each(&:join)
       end
 
-      def write_to_tmp(path, contents)
-        path = "#{out_dir}/#{path}"
+      def write_file_to_disk(path, contents)
+        path = "#{output_directory}/#{path}"
         FileUtils.mkdir_p(File.dirname(path))
         File.write(path, contents)
       end

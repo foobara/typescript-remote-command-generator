bfee73b13df00840304a546bfa2eb86125be143e
Miles Georgi <azimux@gmail.com>
2023-12-22 02:24:08 +0000

    Add a WriteTypescriptToDisk command

--- spec/remote_generator/write_typescript_to_disk_spec.rb
+++ spec/remote_generator/write_typescript_to_disk_spec.rb
@@ -1,23 +1,19 @@
-RSpec.describe Foobara::RemoteGenerator::GenerateTypescript do
+RSpec.describe Foobara::RemoteGenerator::WriteTypescriptToDisk do
   let(:command) { described_class.new(inputs) }
   let(:outcome) { command.run }
   let(:result) { outcome.result }
   let(:errors) { outcome.errors }
-  let(:inputs) { { raw_manifest: } }
+  let(:inputs) { { raw_manifest:, output_directory: } }
+  let(:output_directory) { "#{__dir__}/../../tmp/domains" }
   let(:raw_manifest_json) { File.read("spec/fixtures/foobara-manifest.json") }
   let(:raw_manifest) { JSON.parse(raw_manifest_json) }
 
   it "contains base files" do
     expect(outcome).to be_success
 
-    expect(result.keys).to any match(/base/)
-    expect(command.manifest.organizations.map(&:organization_name)).to include("SomeOrg")
-    expect(command.manifest.domains.map(&:domain_name)).to include("Auth")
-    expect(command.manifest.commands.map(&:command_name)).to include("CreateUser")
-    expect(command.manifest.types.map(&:name)).to include("User")
-    expect(command.manifest.entities.map(&:entity_name)).to include("User")
-
     expect(result["SomeOrg/index.ts"]).to include("export class SomeOrgClass extends Organization {")
     expect(result["SomeOrg/Auth/index.ts"]).to include("export class AuthClass extends Domain {")
+
+    expect(File.exist?("#{output_directory}/foobara-generated.json")).to be true
   end
 end
---
+++ src/remote_generator/write_typescript_to_disk.rb
@@ -0,0 +1,66 @@
+require_relative "generate_typescript"
+
+module Foobara
+  module RemoteGenerator
+    class WriteTypescriptToDisk < Foobara::Command
+      class MissingManifestError < RuntimeError; end
+
+      possible_error MissingManifestError
+
+      # TODO: give better sugar for specifying required inputs
+      inputs raw_manifest: { type: :associative_array },
+             manifest_url: { type: :string },
+             output_directory: { type: :string, required: true }
+      result :associative_array
+
+      depends_on GenerateTypescript
+
+      def execute
+        generate_typescript
+        delete_old_files_if_needed
+        write_all_files_to_disk
+
+        paths_to_source_code
+      end
+
+      attr_accessor :paths_to_source_code
+
+      def generate_typescript
+        # TODO: we need a way to allow values to be nil in type declarations
+        inputs = if raw_manifest
+                   { raw_manifest: }
+                 else
+                   { manifest_url: }
+                 end
+
+        self.paths_to_source_code = run_subcommand!(GenerateTypescript, inputs)
+      end
+
+      def delete_old_files_if_needed
+        file_list_file = "#{output_directory}/foobara-generated.json"
+
+        if File.exist?(file_list_file)
+          file_list = JSON.parse(File.read(file_list_file))
+
+          file_list.map do |file|
+            Thread.new { FileUtils.rm("#{output_directory}/#{file}") }
+          end.each(&:join)
+        end
+      end
+
+      def write_all_files_to_disk
+        write_file_to_disk("foobara-generated.json", paths_to_source_code["foobara-generated.json"])
+
+        paths_to_source_code.map do |path, contents|
+          Thread.new { write_file_to_disk(path, contents) unless path == "foobara-generated.json" }
+        end.each(&:join)
+      end
+
+      def write_file_to_disk(path, contents)
+        path = "#{output_directory}/#{path}"
+        FileUtils.mkdir_p(File.dirname(path))
+        File.write(path, contents)
+      end
+    end
+  end
+end

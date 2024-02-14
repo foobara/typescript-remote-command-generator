9878f7ca9d556b754fb3cd07ab64dc8a4dab4df8
Miles Georgi <azimux@gmail.com>
2024-02-14 02:55:01 +0000

    1707879301

--- src/remote_generator/templates/base/DataPath.ts
+++ src/remote_generator/templates/base/DataPath.ts
@@ -1,17 +1,25 @@
-function _valuesAt(objects: Object[], path: string[]) {
-  if (path.length === 0) return objects;
+function flatten(array: any[]) {
+  return array.reduce((acc, val) => acc.concat(val), []);
+}
+
+function uniq(array: any[]) {
+  return [...new Set(array)];
+}
+
+function _valuesAt(objects: Object[], path: (string | number)[]): Object[] {
+  if (path.length === 0) return objects
 
   const [pathPart, ...remainingParts] = path;
 
   if (pathPart === '#') {
-    objects = [...new Set(objects.flat())];
+    objects = uniq(flatten(objects))
   } else if (typeof pathPart === 'string' || typeof pathPart === 'number') {
-    objects = objects.map((object: Object) => object[pathPart]);
+    objects = objects.map((object: Object) => object[pathPart])
   } else {
-    throw new Error(`Bad path part: ${pathPart}`);
+    throw new Error(`Bad path part: ${pathPart}`)
   }
 
-  return _valuesAt(objects, remainingParts);
+  return _valuesAt(objects, remainingParts)
 }
 
 export function valuesAt(objects: Object[], path: string[]) {

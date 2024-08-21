248ded8cf86ee3c047de629c14f797e7dd36cdab
Miles Georgi <azimux@gmail.com>
2024-08-21 16:17:21 -0700

    Eliminate pointless version.rb test

--- spec/typescript/remote/command/version_spec.rb
+++
@@ -1,5 +0,0 @@
-RSpec.describe Foobara::TypescriptRemoteCommandGenerator::Version do
-  it "has a version number" do
-    expect(Foobara::TypescriptRemoteCommandGenerator::Version::VERSION).to be_a(String)
-  end
-end
